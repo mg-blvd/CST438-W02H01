d93b3e4f4b77147db42e294bb17d7f25e6648f32
mg-blvd <misaelguijarro@gmail.com>
2020-09-03 23:43:39 -0700

    Started editing the main layout and added some strings

--- app/src/main/java/com/example/w02hw01/MainActivity.java
+++ app/src/main/java/com/example/w02hw01/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.w02hw01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.w02hw01.DB.UserDAO;
+import com.example.w02hw01.DB.UserDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private UserDAO mUserDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUserDAO = Room.databaseBuilder(this, UserDatabase.class, UserDatabase.USER_TABLE)
+                .allowMainThreadQueries()
+                .build()
+                .getUserDao();
+
     }
 }
